2958fe369218479983858d01405b716f905fe7b1
Flavio Egoavil <fegoavil010@soa.ad.pwcinternal.com>
2019-02-04 14:07:43 -0500

    two modes: bind and bind-same

--- spinneret-vue-patcher.lisp
+++ spinneret-vue-patcher.lisp
@@ -61,7 +61,7 @@
   (defun valid-attribute? (tag name)
     (or (unvalidated-attribute? name)
         (eql name :attrs)
-        (eql tag :free-tag) ; modification here.
+        (eql tag :free-tag)             ; modification here.
         (global-attribute? name)
         (aria-attribute? name)
         (let ((permitted (permitted-attributes tag)))
@@ -71,29 +71,51 @@
   ;; define tag macro: component :name <component-name>  [attrs] <body>
   ;; if "bind" parameter is present, do a v-bind:xxx on all the parameters to bind.
   ;; I.e.:
-  ;; (component :name "MyComponent" :bind (var1 "var1value" var2 "var2value"))
+  ;; (component :bind (|var1| "value1" |var2| "value2") :name "MyComponent"  "CONTENT")
   ;; -->
-  ;; <MyComponent v-bind:list2=var2 v-bind:list=var>
+  ;; <MyComponent v-bind:var2=value2 v-bind:var1=value1>
   ;; CONTENT
   ;; </MyComponent>
 
+  ;; other usage: :bind-same attr
+  ;; (component :bind-same (|var1| |var2|) :name "MyComponent"  "CONTENT"))
+
+  ;;  <MyComponent v-bind:var2=var2 v-bind:var1=var1>
+  ;;   CONTENT
+  ;;  </MyComponent>
   
   (deftag component (body attrs)
-    (if (not (getf attrs :bind))
-        `(:free-tag ,@attrs ,@body)
-        ;; use the bind list
-        (progn
-             (let ((a attrs)) ;modified attrs
-               (loop for (key value) on (getf attrs :bind) by #'cddr
-                     do
-                     (push value a)
-                     (push (intern (format nil "v-bind:~a" key) :keyword) ;create keyword
-                           a))
-               (remf a :bind) ;remove :bind prop on our property list
-               
-               `(:free-tag ,@a 
-                          ,@body))))
-        ))
+    (cond
+      ((not (or (getf attrs :bind)
+                (getf attrs :bind-same)))
+       ;; no bind attr, no bind-same attr
+       `(:free-tag ,@attrs ,@body))
+      ((getf attrs :bind)
+       ;; use the bind list
+       (progn
+         (let ((a attrs))               ;modified attrs
+           (loop for (key value) on (getf attrs :bind) by #'cddr
+                 do
+                 (push value a)
+                 (push (intern (format nil "v-bind:~a" key) :keyword) ;create keyword
+                       a))
+           (remf a :bind)      ;remove :bind prop on our property list
+           
+           `(:free-tag ,@a 
+                       ,@body))))
+      ((getf attrs :bind-same)
+       ;; bind to vars of the same name.
+       (progn
+         (let ((a attrs))               ;modified attrs
+           (loop for key in (getf attrs :bind-same) 
+                 do
+                 (push (symbol-name key) a)
+                 (push (intern (format nil "v-bind:~a" key) :keyword) ;create keyword
+                       a))
+           (remf a :bind-same) 
+           
+           `(:free-tag ,@a 
+                       ,@body)))))))
 
 
 (eval-when (:compile-toplevel :load-toplevel :execute)
